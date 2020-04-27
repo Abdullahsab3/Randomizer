22ed128b8419a14c80bd15d87177d937000acc33
Abdullahsab3 <36844223+Abdullahsab3@users.noreply.github.com>
2020-04-27 10:54:50 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import random
-
-elements = ["0", "1", "2", "3", "4", "5", "6" "7", "8", "9", "a", "b", "c", "d", "e", "f"]
-code = "#"
-for i in range(0, 6):
-    code = code + random.choice(elements)
-print(code)
